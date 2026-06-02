d20e1b5dd6c97c0d334c64f37aa8ca90a4937927
Reese Wang <eseer.rw@gmail.com>
2026-06-02 13:42:53 -0400

    deleted unnecessary files

--- scripts/parse_all.py
+++
@@ -1,9 +0,0 @@
-from parser import parse_all_citations
-
-block = [ 
-    ""
-
-]
-
-for r in parse_all_citations(block):
-    print(r.title, r.year, r.authors, r.journal)
--- scripts/testing.py
+++
@@ -1,8 +0,0 @@
-from parser import parse_citation
-
-c = parse_citation("Brandau S, Jakob M, Bruderek K, Bootz F, Giebel B, Radtke S, Mauel K, Jäger M, Flohé SB, Lang S (2014) Mesenchymal stem cells augment the anti-bacterial activity of neutrophil granulocytes. PLoS ONE 9:e106903 [DOI] [PMC free article] [PubMed] [Google Scholar]")
-
-print("style:  ", c.style)
-print("title:  ", c.title)
-print("year:   ", c.year)
-print("journal:", c.journal)
